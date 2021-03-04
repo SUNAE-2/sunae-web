732142f5f04a245f978ef30a8d98e3701e613ecc
Diego Jiménez Urgell <diegourgell@outlook.com>
2021-03-04 01:23:14 -0600

    #5 El contenido de las FAQ ya es dinámico, cada pregunta tiene un checkbox en admin para activarla

--- sunaeweb/index/models.py
+++ sunaeweb/index/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Pregunta(models.Model):
+    pregunta = models.CharField(max_length=1000)
+    respuesta = models.CharField(max_length=1000)
+    activa = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.pregunta
--- sunaeweb/index/views.py
+++ sunaeweb/index/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
+from .models import Pregunta
 
 # Create your views here.
 def home(request, *args, **kwargs):
     return render(request, 'home.html', {})
 
 def faq(request, *args, **kwargs):
-    return render(request, 'faq.html')
+    ques = Pregunta.objects.filter(activa__exact=True)
+    context = {
+        'ques': ques
+    }
+    return render(request, 'faq.html', context=context)
 
 def portfolio(request, *args, **kwargs):
     return render(request, 'portfolio-details.html')
 
 def inner_page(request, *args, **kwargs):
     return render(request, 'inner-page.html', {})
+
+
