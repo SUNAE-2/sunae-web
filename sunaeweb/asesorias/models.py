a166744085b8b83c30a8debb6e8c3c853d5f8348
Daniel Yamamoto <danylarayamamoto@hotmail.com>
2021-03-04 20:37:19 -0600

    #6 Inicio Asesorias

--- sunaeweb/asesorias/models.py
+++ sunaeweb/asesorias/models.py
@@ -1,3 +1,14 @@
+from carreras.models import Carrera
 from django.db import models
 
 # Create your models here.
+class Asesorias(models.Model):
+    fullname = models.CharField(max_length=20)
+    description = models.TextField()
+    email = models.EmailField()
+    carrera= models.ForeignKey(Carrera, on_delete= models.CASCADE)
+    active = models.BooleanField()
+    def __str__(self):
+        return self.fullname
+
+
--- sunaeweb/asesorias/views.py
+++ sunaeweb/asesorias/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def asesorias(request, *args, **kwargs):
-    return render(request, 'home.html', {})
+    return render(request, 'formularios.html', {})
