7d6fde7b5dc12567e4d7528b96c1068af886f007
aizzo01 <a01028454@itesm.mx>
2021-03-04 20:12:24 -0600

    Modelos Carreras #7 ya hice la base de datos de carrera y sirve, pero no imprime en el home

--- sunaeweb/carreras/models.py
+++ sunaeweb/carreras/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 
 class Carrera(models.Model):
-    carreer = models.CharField(max_length=20)
-    instructor = models.TextField()
-    correo = models.TextField()
-    active = models.BooleanField()
+    nombre = models.CharField(max_length=20)
+    descripcion = models.TextField()
+    activo = models.BooleanField()
 
     def __str__(self):
-        return self.carreer
+        return self.nombre
+
+
+class Instructor(models.Model):
+    carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=20)
+    correo = models.CharField(max_length=20)
+    activo = models.BooleanField()
+
+    def __str__(self):
+        return self.nombre
+
+
