4e3ddec39f205aad616885036be66e198a388c76
Daniel Yamamoto <danylarayamamoto@hotmail.com>
2021-05-21 23:54:10 -0500

    # Rebranding, responsive design, color nav changed

--- sunaeweb/assets/js/getColor.js
+++ sunaeweb/assets/js/getColor.js
@@ -1,3 +1,7 @@
 const colors = ['#FF9D37', '#3974B7', '#3292F9'];
-const randomColor = colors[Math.floor(Math.random() * colors.length)];
-document.querySelector('.title').style.color = randomColor;
+const elements = document.getElementsByClassName('title');
+
+for(let i = 0; i < elements.length; i++) {
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    elements[i].style.color = randomColor;
+}
